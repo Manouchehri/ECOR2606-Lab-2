bfa508fbbcdf70ec9e858392f67874990702f8d7
David Manouchehri <david.manouchehri@carleton.ca>
2016-01-25 10:17:03 -0500

    Checkpoint.

--- MATLAB/overlap.m
+++ MATLAB/overlap.m
@@ -9,7 +9,18 @@ function[ overlap ] = overlap(R1, R2, D)
 
 % circcirc
 
-A = @(x, R) pi * R^2 / 2 - (x * sqrt(R^2 - x^2) + R^2 * arcsin(x / R))
+A = @(x, R) pi * R^2 / 2 - (x * sqrt(R^2 - x^2) + R^2 * asin(x / R));
+
+x1 = (D^2 + R1^2 - R2^2)/(2*D);
+x2 = (D - x1);
+
+if(min(R1, R2) > D)    
+    % They overlap
+    overlap = A(D, R1) + A(D, R2);
+else
+    error;
+    % They don't overlap
+end
 
 % Do stuff here.
 
