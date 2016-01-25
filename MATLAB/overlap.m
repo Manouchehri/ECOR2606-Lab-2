a6361bda95248b4385803ef5c4bf51ad89b645e1
David Manouchehri <david.manouchehri@carleton.ca>
2016-01-25 10:40:32 -0500

    Done?

--- MATLAB/overlap.m
+++ MATLAB/overlap.m
@@ -1,24 +1,23 @@
 % ECOR2606 - David Manouchehri
 
-function[ overlap ] = overlap(R1, R2, D)
+function[ area ] = overlap(R1, R2, D)
 % overlap   Given a
 % Inputs:   R1 = 
 %           R2 =
 %       	D  =
-% Outputs:  overlap
+% Outputs:  area
 
 % circcirc
 
-A = @(x, R) pi * R^2 / 2 - (x * sqrt(R^2 - x^2) + R^2 * asin(x / R));
-
-x1 = (D^2 + R1^2 - R2^2)/(2*D);
-x2 = (D - x1);
-
 if(min(R1, R2) > D)    
     % They overlap
-    overlap = A(D, R1) + A(D, R2);
+    A = @(x, R) pi .* R.^2 / 2 - (x .* sqrt(R.^2 - x.^2) + R.^2 .* asin(x ./ R));
+
+    x1 = (D^2 + R1^2 - R2^2)/(2*D);
+    x2 = (D - x1);
+    area = A(x1, R1) + A(x2, R2);
 else
-    error;
+    error('Circles do not overlap!');
     % They don't overlap
 end
 
