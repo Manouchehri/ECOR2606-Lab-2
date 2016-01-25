bb180c9abc545c39a7aebb0bbbfc45cad5e9936f
David Manouchehri <david@davidmanouchehri.com>
2016-01-25 23:07:47 +0000

    Do elementwise math.

--- MATLAB/overlap.m
+++ MATLAB/overlap.m
@@ -22,7 +22,7 @@ elseif(D >= R1 + R2) % No overlap, circles too far apart.
     area = 0;
 else
     A = @(x, R) pi .* R.^2 / 2 - (x .* sqrt(R.^2 - x.^2) + R.^2 .* asin(x ./ R));
-    x1 = (D^2 + R1^2 - R2^2)/(2*D);
+    x1 = (D.^2 + R1^2 - R2^2)./(2*D);
     x2 = (D - x1);
     area = A(x1, R1) + A(x2, R2);
 end
